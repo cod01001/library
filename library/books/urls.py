f87d61b3c294478462507b0d57820794f83f9989
Mark <smailsist766@gmail.com>
2024-02-14 22:32:45 -0500

    9

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.books_index, name="books_index"),
-    path("<int:pk>/", views.books_detail, name="books_detail"),
-    path("<category>", views.books_category, name="books_category"),
-
+    path("book/<int:pk>/", views.books_detail, name="books_detail"),
+    path("category/<str:category>/", views.books_category, name="books_category"),
+    path("author/<str:author>/", views.books_name_author, name="books_author"),
 ]
-
